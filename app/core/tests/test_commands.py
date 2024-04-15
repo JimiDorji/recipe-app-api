4f4f10010e75cd9cfde0619a347759d2eafb1e68
JimiDorji <jigmedorji1000@gmail.com>
2024-04-15 16:53:58 +0600

    line removed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,7 +13,6 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
@@ -27,7 +26,6 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
-
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
