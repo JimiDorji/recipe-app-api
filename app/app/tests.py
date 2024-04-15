0c45ba8d8d3fa3ad6db9e6ab6aed8d8a0c0e7c5d
JimiDorji <jigmedorji1000@gmail.com>
2024-04-15 15:02:39 +0600

    added pythoncylopg2

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """ Test the calc module."""
+
+    def test_add_numbers(self):
+        """ Test the add_numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+        
+    def test_subtract_numbers(self):
+        """ Test the subtract_numbers together."""
+        res = calc.subtract(10, 15)	
+        
+        self.assertEqual(res, 5)
