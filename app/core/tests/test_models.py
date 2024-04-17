a502e2ceab72237cd32ec5ea173a9d32681ead03
JimiDorji <jigmedorji1000@gmail.com>
2024-04-17 16:13:31 +0600

    refactoring

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,12 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
 
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """Test the models."""
 
@@ -20,7 +26,6 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
@@ -64,3 +69,20 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
